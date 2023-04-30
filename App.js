a1f4aee9e5ebb37013059579fd41026bfa5bcb90
jscodebit <soujanya.janapatla@gmail.com>
2023-04-30 13:08:29 -0400

    React introduction

---
+++ App.js
@@ -0,0 +1,7 @@
+const header = React.createElement("div", { id: "heading", class: "red"}, 
+                                            [React.createElement("h1", {}, "H1 Tag"), 
+                                            React.createElement("h2", {}, "H2 Tag")]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(header);
+root.render(header);
