c9f3e2b6bf9bef7086ec143e514a96dab13a4de9
jscodebit <soujanya.janapatla@gmail.com>
2023-05-12 01:23:21 -0400

    Learning JSX and React.Fragment

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const header = React.createElement("div", { id: "heading", class: "red"}, 
-                                            [React.createElement("h1", {}, "H1 Tag"), 
-                                            React.createElement("h2", {}, "H2 Tag")]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const header = React.createElement("h1", { id: "app-header"}, "Hello world!");
 
-console.log(header);
-root.render(header);
+//JSX is transpiled before it reaches the JS Engine - Parcel - Babel
+//JSX = > React.createElement => React element to JS Object   => HTML Element (render)
+const jsxHeader = <h1 id="app-header">Hello world!</h1>
+console.log(jsxHeader);
+console.log(jsxHeader == header)
+
+
+//React Components
+//Class based Components - OLD
+//Functional Components - NEW
+const Title = () => (
+    <h1>React functional Component</h1>
+);
+const Header = () => (
+    <div className="container">
+        <Title/>
+    </div>
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header />);
