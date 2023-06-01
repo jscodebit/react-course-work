278ccf6e357ccc8f4f503706cc284ee6c8e91751
jscodebit <soujanya.janapatla@gmail.com>
2023-05-31 21:46:47 -0400

    Comparing Class based component life cycle with functional component

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,29 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
 import ProfileClassCommponent from "./ProfileClass";
 import ProfileFunctionalComponent from "./Profile";
 
-const About = () => {
-    return (
-        <div>
-            <h1>About Us Page</h1>
-            {/* <Outlet/> */}
-            <ProfileClassCommponent name={"Soujanya"}/>
-            <ProfileFunctionalComponent name={"Akshay"}/>
-        </div>
-    )
+class About extends React.Component {
+    constructor(props){
+        super(props);
+        console.log("Parent constructor");
+
+    }
+    componentDidMount(){
+        console.log("Parent componentDidMount");
+    }
+    render(){
+        console.log("Parent render");
+        return (
+            <div>
+                <h1>About Us Page</h1>
+                {/* <Outlet/> */}
+                <ProfileClassCommponent name={"Child 1"}/>
+                <ProfileClassCommponent name={"Child 2"}/>
+                {/* <ProfileFunctionalComponent name={"Akshay"}/> */}
+            </div>
+        )
+    }
 }
+
 export default About;
--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -3,22 +3,47 @@ import React from "react";
 class Profile extends React.Component {
     constructor(props){
         super(props);
+        console.log("Child Constructor class component"+ this.props.name);
         this.state = {
-            count: 0
+            count: 0,
+            userInfo: {
+                login: '',
+                avatar_url: '',
+                location: '',
+                followers: 0
+            }
         }
-        console.log("Constructor class component");
     }
 
-    componentDidiMount(){
-        console.log("componentDidMount");
+    componentDidMount(){
+        console.log("Child componentDidMount" + this.props.name);
+        // const user = await fetch("https://api.github.com/users/jscodebit");
+        // const json = await user.json();
+        // // console.log(json);
+        // this.setState({
+        //     userInfo: json
+        // })
+        
     }
+
+    componentDidUpdate(){
+        console.log("Child componentDidUpdate");
+    }
+
+    componentWillUnmount(){
+        console.log("Child will unmount");
+    }
+
     render(){
         const { count } = this.state;
-        console.log("Render class component"); 
+        console.log("Child Render class component"+ this.props.name); 
         return (
             <div>
                 <h2>Profile Class Component</h2>
-                <h3>Name : {this.props.name}</h3>
+                <h3>Name : {this.state.userInfo.login}</h3>
+                <img src={this.state.userInfo.avatar_url}/>
+                <h3>Location : {this.state.userInfo.location}</h3>
+                <h3>followers : {this.state.userInfo.followers}</h3>
                 <h4>Count :  {count}</h4>
                 <button onClick={() => {
                     this.setState({count: count +1})
